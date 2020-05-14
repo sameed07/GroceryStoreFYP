52060d8aa1d0f888f8435a9b99ad6b4115babab0
Sameed <sameedshah9@gmail.com>
2020-05-14 12:47:29 +0500

    google signin added

---
+++ app/src/main/java/Activities/MainActivity.java
@@ -0,0 +1,28 @@
+package Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.infusibleCoder.grocerystorefyp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_login,btn_singup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+    }
+}
